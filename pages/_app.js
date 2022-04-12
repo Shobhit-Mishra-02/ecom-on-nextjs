ed0441ef0826227be01eed48cc59cdf9a13e1536
shobhit <shobhitmishra2002@gmail.com>
2022-04-12 22:00:54 +0530

    added some inital code for the development of navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Navigation from "../components/Nav";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="mx-[12px]">
+        <Navigation />
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
