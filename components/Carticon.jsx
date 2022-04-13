5266d42cca90d7067395c118bf8bfc9763d91e79
shobhit <shobhitmishra2002@gmail.com>
2022-04-13 08:49:57 +0530

    addes code for the navigation bar to make responsive for the mobile and tablet

---
+++ components/Carticon.jsx
@@ -0,0 +1,25 @@
+const CartIcon = () => {
+  return (
+    <div className="w-fit p-1 relative cursor-pointer">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-8 w-8"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+        />
+      </svg>
+      <span className="bg-blue-500 absolute rounded-full px-2 text-white text-sm font-semibold top-0 right-0">
+        1
+      </span>
+    </div>
+  );
+};
+
+export default CartIcon;
